5f70abba8f8581ba8d54492cf306ed5b5e8b25f0
gggggdw <101677049+gggggdw@users.noreply.github.com>
2025-04-12 21:16:17 +0800

    Update Configuration.java

--- src/main/java/com/wildfire/main/config/Configuration.java
+++ src/main/java/com/wildfire/main/config/Configuration.java
@@ -38,7 +38,7 @@ public class Configuration extends AbstractConfiguration {
 
 	public static final BooleanConfigKey BREAST_PHYSICS = new BooleanConfigKey("breast_physics", true);
 	public static final BooleanConfigKey ARMOR_PHYSICS_OVERRIDE = new BooleanConfigKey("armor_physics_override", false);
-	public static final BooleanConfigKey BREAST_PHYSICS_ARMOR = new BooleanConfigKey("breast_physics_armor", false);
+	public static final BooleanConfigKey BREAST_PHYSICS_ARMOR = new BooleanConfigKey("breast_physics_armor", true);
 	public static final BooleanConfigKey SHOW_IN_ARMOR = new BooleanConfigKey("show_in_armor", true);
 	public static final FloatConfigKey BOUNCE_MULTIPLIER = new FloatConfigKey("bounce_multiplier", 0.333F, 0, 1.0f);
 	public static final FloatConfigKey FLOPPY_MULTIPLIER = new FloatConfigKey("floppy_multiplier", 0.75F, 0.5f, 1);
